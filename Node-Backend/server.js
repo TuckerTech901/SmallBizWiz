80efb0418f86741551e3f4c16fea8f3e9835a6f7
TuckerTech901 <TuckerTech901@gmail.com>
2023-03-01 12:27:53 -0500

    Connecting to cloud database

--- Node-Backend/server.js
+++ Node-Backend/server.js
@@ -4,6 +4,12 @@ const express = require('express');
 const bodyParser = require ('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
+const connectionString= 'mongodb+srv://katrinatucker901:Leland1941@smallbizzwizz.soa3spq.mongodb.net/?retryWrites=true&w=majority'
+
+MongoClient.connect(connectionString, (err, client) => {
+  if (err) return console.error(err)
+  console.log('Connected to Database')
+})
 
 
 //Always add your body-parser before your CRUD handlers!
