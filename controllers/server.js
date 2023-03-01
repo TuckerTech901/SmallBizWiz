16ea7bbda991350c2c8f0e8bd91438d4a7f121bd
TuckerTech901 <TuckerTech901@gmail.com>
2023-02-28 22:15:51 -0500

    moved main server to controllers folder

---
+++ controllers/server.js
@@ -0,0 +1,8 @@
+console.log('May Node be with You')
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+    console.log('listening on 3000')
+  })
--- server.js
+++
@@ -1,1 +0,0 @@
-console.log('May Node be with You')
