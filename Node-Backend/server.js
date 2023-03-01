1cc60a7d50cedd7179b0b7f94bb30e2a7fc61131
TuckerTech901 <TuckerTech901@gmail.com>
2023-02-28 23:53:44 -0500

    adding body parser dependency

--- Node-Backend/server.js
+++ Node-Backend/server.js
@@ -1,7 +1,8 @@
 //console.log('May Node be with You')
 
 const express = require('express');
-const app = express();
+const bodyParser = require ('body-parser')
+const app = express()
 
 app.listen(3000, function() {
     console.log('listening on 3000')
@@ -10,6 +11,8 @@ app.listen(3000, function() {
 //   app.get('/', function(req, res) {
 //     res.send('Hello World')
 //   })
+
+  app.use(bodyParser.urlencoded({ extended: true }))
 
   app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
